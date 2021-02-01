fcee38ffccd4dedbbab856b0e23b0cac81e2bc92
stayQuite <h4vefun.dieyoung@gmail.com>
2021-02-01 03:55:57 +0200

    fix: fix description-area min-width

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
-let header = document.querySelector('.header-area');
+let header = document.querySelector('.header-area'),
+    about = document.querySelector('#about'),
+    education = document.querySelector('#education');
 header.addEventListener('click', (event) => {
-    if(event.target.tagName != 'A' || event.target.href != "") return
-    about.classList.toggle('hide');
-    education.classList.toggle('hide');
+    let id = event.target.id;
+    if(id != 'eduButton' && id != 'aboutButton') return
+    event.preventDefault();
+    if(id == 'eduButton' && education.classList.contains('hide')){
+        education.classList.toggle('hide');
+        about.classList.toggle('hide');
+    }
+    if(id == 'aboutButton' && about.classList.contains('hide')){
+    }
 });
-zs
