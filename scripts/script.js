0e51dabc36ed869005e60666d9233f0145f0be73
stayQuite <h4vefun.dieyoung@gmail.com>
2021-02-08 20:01:01 +0200

    feat: add adaptive font size to header/add git icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,34 @@
 let header = document.querySelector('.header-area'),
-    about = document.querySelector('#about'),
-    education = document.querySelector('#education');
+    about = document.querySelector('.about'),
+    education = document.querySelector('.education'),
+    code = document.querySelector('.code');
+
 header.addEventListener('click', (event) => {
     let id = event.target.id;
-    if(id != 'eduButton' && id != 'aboutButton') return
+    if(id != 'eduButton' && id != 'aboutButton' && id != 'codeButton') return
     event.preventDefault();
-    if(id == 'eduButton' && education.classList.contains('hide')){
-        education.classList.toggle('hide');
-        about.classList.toggle('hide');
+    if(id == 'eduButton' && !education.classList.contains('active')){
+        show(education);
     }
-    if(id == 'aboutButton' && about.classList.contains('hide')){
-        about.classList.toggle('hide');
-        education.classList.toggle('hide');
+    if(id == 'aboutButton' && !about.classList.contains('active')){
+        show(about);
+    }
+    if(id == 'codeButton' && !code.classList.contains('active')){
+        show(code);
     }
 });
+
+function show(element){
+    let active = document.querySelector('.active');
+    active.addEventListener('transitionend', reActive);
+    active.classList.remove('active');
+    active.classList.add('hidden');
+    function reActive(){
+        active.style.display = 'none';
+        element.style.display = 'block';
+        setTimeout(() => {
+            element.classList.remove('hidden');
+            element.classList.add('active')}, 0);
+            active.removeEventListener('transitionend', reActive);
+    }
+};
